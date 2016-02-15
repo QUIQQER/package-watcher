8c26459f931e30e2520cd19264f2b8a565f67125
Henning Leutz <leutz@pcsg.de>
2016-02-15 07:58:54 +0100

    fa 4.5

--- bin/Panel.js
+++ bin/Panel.js
@@ -1,4 +1,3 @@
-
 /**
  * Watcher panel
  *
@@ -30,18 +29,17 @@ define('package/quiqqer/watcher/bin/Panel', [
 
     'css!package/quiqqer/watcher/bin/Panel.css'
 
-], function(QUI, QUIPanel, QUIAttention, QUIFormUtils, Grid, Template, ControlUtils, QUILocale, QUIAjax)
-{
+], function (QUI, QUIPanel, QUIAttention, QUIFormUtils, Grid, Template, ControlUtils, QUILocale, QUIAjax) {
     "use strict";
 
     var lg = 'quiqqer/watcher';
 
     return new Class({
 
-        Extends : QUIPanel,
-        Type : 'package/quiqqer/watcher/bin/Panel',
+        Extends: QUIPanel,
+        Type   : 'package/quiqqer/watcher/bin/Panel',
 
-        Binds : [
+        Binds: [
             'loadData',
             'openSearch',
             'openClear',
@@ -49,11 +47,10 @@ define('package/quiqqer/watcher/bin/Panel', [
             '$onResize'
         ],
 
-        initialize : function(option)
-        {
+        initialize: function (option) {
             this.setAttributes({
-                icon : 'icon-eye-open',
-                title : QUILocale.get('quiqqer/watcher', 'panel.title')
+                icon : 'fa fa-eye',
+                title: QUILocale.get('quiqqer/watcher', 'panel.title')
             });
 
             this.parent(option);
@@ -61,32 +58,31 @@ define('package/quiqqer/watcher/bin/Panel', [
             this.$Grid = null;
 
             this.addEvents({
-                onCreate : this.$onCreate,
-                onResize : this.$onResize,
-                onInject : this.$onInject
+                onCreate: this.$onCreate,
+                onResize: this.$onResize,
+                onInject: this.$onInject
             });
         },
 
         /**
          * event : on create
          */
-        $onCreate : function()
-        {
+        $onCreate: function () {
             // Buttons
             this.addButton({
-                text : QUILocale.get(lg, 'panel.button.search.title'),
-                textimage : 'icon-search',
-                events : {
-                    onClick : this.openSearch
+                text     : QUILocale.get(lg, 'panel.button.search.title'),
+                textimage: 'fa fa-search',
+                events   : {
+                    onClick: this.openSearch
                 }
             });
 
             // Buttons
             this.addButton({
-                text : QUILocale.get(lg, 'panel.button.clear.title'),
-                textimage : 'icon-eraser',
-                events : {
-                    onClick : this.openClear
+                text     : QUILocale.get(lg, 'panel.button.clear.title'),
+                textimage: 'fa fa-eraser',
+                events   : {
+                    onClick: this.openClear
                 }
             });
 
@@ -96,41 +92,41 @@ define('package/quiqqer/watcher/bin/Panel', [
             );
 
             this.$Grid = new Grid(Container, {
-                columnModel : [{
-                    header    : QUILocale.get('quiqqer/system', 'id'),
-                    dataIndex : 'id',
-                    dataType  : 'number',
-                    width     : 60
+                columnModel: [{
+                    header   : QUILocale.get('quiqqer/system', 'id'),
+                    dataIndex: 'id',
+                    dataType : 'number',
+                    width    : 60
                 }, {
-                    header    : QUILocale.get('quiqqer/system', 'user_id'),
-                    dataIndex : 'uid',
-                    dataType  : 'number',
-                    width     : 100
+                    header   : QUILocale.get('quiqqer/system', 'user_id'),
+                    dataIndex: 'uid',
+                    dataType : 'number',
+                    width    : 100
                 }, {
-                    header    : QUILocale.get('quiqqer/system', 'username'),
-                    dataIndex : 'username',
-                    dataType  : 'number',
-                    width     : 100
+                    header   : QUILocale.get('quiqqer/system', 'username'),
+                    dataIndex: 'username',
+                    dataType : 'number',
+                    width    : 100
                 }, {
-                    header    : QUILocale.get('quiqqer/system', 'date'),
-                    dataIndex : 'statusTime',
-                    dataType  : 'date',
-                    width     : 140
+                    header   : QUILocale.get('quiqqer/system', 'date'),
+                    dataIndex: 'statusTime',
+                    dataType : 'date',
+                    width    : 140
                 }, {
-                    header    : QUILocale.get(lg, 'grid.message'),
-                    dataIndex : 'message',
-                    dataType  : 'string',
-                    width     : 300
+                    header   : QUILocale.get(lg, 'grid.message'),
+                    dataIndex: 'message',
+                    dataType : 'string',
+                    width    : 300
                 }, {
-                    header    : QUILocale.get(lg, 'grid.call'),
-                    dataIndex : 'call',
-                    dataType  : 'string',
-                    width     : 200
+                    header   : QUILocale.get(lg, 'grid.call'),
+                    dataIndex: 'call',
+                    dataType : 'string',
+                    width    : 200
                 }, {
-                    header    : QUILocale.get(lg, 'grid.callParams'),
-                    dataIndex : 'callParams',
-                    dataType  : 'string',
-                    width     : 200
+                    header   : QUILocale.get(lg, 'grid.callParams'),
+                    dataIndex: 'callParams',
+                    dataType : 'string',
+                    width    : 200
                 }],
                 onrefresh  : this.loadData,
                 pagination : true,
@@ -147,16 +143,14 @@ define('package/quiqqer/watcher/bin/Panel', [
         /**
          * event : on inject
          */
-        $onInject : function()
-        {
+        $onInject: function () {
             this.loadData();
         },
 
         /**
          * event : on resize
          */
-        $onResize : function()
-        {
+        $onResize: function () {
             if (!this.$Grid) {
                 return;
             }
@@ -168,7 +162,7 @@ define('package/quiqqer/watcher/bin/Panel', [
             }
 
 
-            var size = Content.getSize(),
+            var size      = Content.getSize(),
                 Attention = Content.getElement('.messages-message');
 
             if (Attention) {
@@ -183,15 +177,14 @@ define('package/quiqqer/watcher/bin/Panel', [
         /**
          * Load the grid data
          */
-        loadData : function()
-        {
+        loadData: function () {
             if (!this.$Grid) {
                 return;
             }
 
             this.Loader.show();
 
-            var self = this;
+            var self    = this;
             var options = this.$Grid.options;
 
             var search = false;
@@ -200,39 +193,37 @@ define('package/quiqqer/watcher/bin/Panel', [
                 search = JSON.encode(this.getAttribute('search'));
             }
 
-            QUIAjax.get('package_quiqqer_watcher_ajax_list', function(result)
-            {
+            QUIAjax.get('package_quiqqer_watcher_ajax_list', function (result) {
                 self.$Grid.setData(result);
                 self.Loader.hide();
 
             }, {
-                'package' : 'quiqqer/watcher',
-                params : JSON.encode({
-                    sortOn  : options.sortOn,
-                    sortBy  : options.sortBy,
-                    perPage : options.perPage,
-                    page    : options.page
+                'package': 'quiqqer/watcher',
+                params   : JSON.encode({
+                    sortOn : options.sortOn,
+                    sortBy : options.sortBy,
+                    perPage: options.perPage,
+                    page   : options.page
                 }),
-                search : search
+                search   : search
             });
         },
 
         /**
          * Open search sheet
          */
-        openSearch : function()
-        {
+        openSearch: function () {
             var self = this;
 
             var Sheet = this.createSheet({
-                title : QUILocale.get(lg, 'panel.search.title'),
-                icon : 'icon-search',
-                closeButton : {
-                    textimage : 'icon-remove fa fa-remove',
-                    text      : QUILocale.get(lg, 'panel.search.button.close')
+                title      : QUILocale.get(lg, 'panel.search.title'),
+                icon       : 'fa fa-search',
+                closeButton: {
+                    textimage: 'fa fa-remove',
+                    text     : QUILocale.get(lg, 'panel.search.button.close')
                 },
-                events : {
-                    onOpen : function(Sheet) {
+                events     : {
+                    onOpen: function (Sheet) {
 
                         self.Loader.show();
 
@@ -241,12 +232,12 @@ define('package/quiqqer/watcher/bin/Panel', [
 
                         Content.addClass('quiqqer-watcher-search');
 
-                        Template.get('bin/PanelSearch', function(result) {
+                        Template.get('bin/PanelSearch', function (result) {
 
                             Content.set({
-                                html : result,
-                                styles : {
-                                    padding : 20
+                                html  : result,
+                                styles: {
+                                    padding: 20
                                 }
                             });
 
@@ -255,32 +246,32 @@ define('package/quiqqer/watcher/bin/Panel', [
                                 Content.getElement('form')
                             );
 
-                            ControlUtils.parse(Content).then(function() {
+                            ControlUtils.parse(Content).then(function () {
                                 self.Loader.hide();
                             });
 
                         }, {
-                            'package' : 'quiqqer/watcher'
+                            'package': 'quiqqer/watcher'
                         });
                     }
                 }
             });
 
             Sheet.addButton({
-                text      : QUILocale.get(lg, 'panel.search.button.search'),
-                textimage : 'icon-search',
-                events : {
-                    onClick : function() {
+                text     : QUILocale.get(lg, 'panel.search.button.search'),
+                textimage: 'fa fa-search',
+                events   : {
+                    onClick: function () {
 
                         var data = QUIFormUtils.getFormData(
                             Sheet.getContent().getElement('form')
                         );
 
                         self.setAttributes({
-                            search : {
-                                uid  : data.uid,
-                                from : data.from,
-                                to   : data.to
+                            search: {
+                                uid : data.uid,
+                                from: data.from,
+                                to  : data.to
                             }
                         });
 
@@ -298,19 +289,18 @@ define('package/quiqqer/watcher/bin/Panel', [
         /**
          * open clear sheet
          */
-        openClear : function()
-        {
+        openClear: function () {
             var self = this;
 
             var Sheet = this.createSheet({
-                title : QUILocale.get(lg, 'panel.clear.title'),
-                icon : 'icon-search',
-                closeButton : {
-                    textimage : 'icon-remove fa fa-remove',
-                    text      : QUILocale.get('quiqqer/system', 'cancel')
+                title      : QUILocale.get(lg, 'panel.clear.title'),
+                icon       : 'fa fa-search',
+                closeButton: {
+                    textimage: 'fa fa-remove',
+                    text     : QUILocale.get('quiqqer/system', 'cancel')
                 },
-                events : {
-                    onOpen : function(Sheet) {
+                events     : {
+                    onOpen: function (Sheet) {
 
                         self.Loader.show();
 
@@ -318,36 +308,36 @@ define('package/quiqqer/watcher/bin/Panel', [
 
                         Content.addClass('quiqqer-watcher-clear');
 
-                        Template.get('bin/PanelClear', function(result) {
+                        Template.get('bin/PanelClear', function (result) {
 
                             Content.set({
-                                html : result,
-                                styles : {
-                                    padding : 20
+                                html  : result,
+                                styles: {
+                                    padding: 20
                                 }
                             });
 
-                            ControlUtils.parse(Content).then(function() {
+                            ControlUtils.parse(Content).then(function () {
                                 self.Loader.hide();
                             });
 
                         }, {
-                            'package' : 'quiqqer/watcher'
+                            'package': 'quiqqer/watcher'
                         });
                     }
                 }
             });
 
             Sheet.addButton({
-                text      : QUILocale.get('quiqqer/system', 'panel.clear.btn.execute'),
-                textimage : 'icon-eraser',
-                events : {
-                    onClick : function() {
+                text     : QUILocale.get('quiqqer/system', 'panel.clear.btn.execute'),
+                textimage: 'fa fa-eraser',
+                events   : {
+                    onClick: function () {
 
                         self.Loader.show();
 
                         var Content = Sheet.getContent(),
-                            date = '';
+                            date    = '';
 
                         if (Content.getElement('[name="watcher-clear-date"]')) {
                             date = Content.getElement('[name="watcher-clear-date"]').value;
@@ -358,14 +348,14 @@ define('package/quiqqer/watcher/bin/Panel', [
                             return;
                         }
 
-                        QUIAjax.post('package_quiqqer_watcher_ajax_clear', function() {
+                        QUIAjax.post('package_quiqqer_watcher_ajax_clear', function () {
 
                             Sheet.hide();
                             self.loadData();
 
                         }, {
-                            'package' : 'quiqqer/watcher',
-                            date : date
+                            'package': 'quiqqer/watcher',
+                            date     : date
                         });
 
                     }
@@ -378,8 +368,7 @@ define('package/quiqqer/watcher/bin/Panel', [
         /**
          * Show search display
          */
-        showSearchDisplay : function()
-        {
+        showSearchDisplay: function () {
             if (this.getContent().getElement('.messages-message')) {
                 return;
             }
@@ -387,11 +376,9 @@ define('package/quiqqer/watcher/bin/Panel', [
             var self = this;
 
             new QUIAttention({
-                message : QUILocale.get(lg, 'grid.search.info'),
-                events  :
-                {
-                    onClick : function(Message)
-                    {
+                message: QUILocale.get(lg, 'grid.search.info'),
+                events : {
+                    onClick: function (Message) {
                         self.setAttribute('search', false);
 
                         Message.destroy();
@@ -400,15 +387,14 @@ define('package/quiqqer/watcher/bin/Panel', [
                         self.$onResize();
                     }
                 },
-                styles  : {
-                    margin : '0 0 20px',
-                    'border-width' : 1,
-                    cursor : 'pointer'
+                styles : {
+                    margin        : '0 0 20px',
+                    'border-width': 1,
+                    cursor        : 'pointer'
                 }
             }).inject(this.getContent(), 'top');
 
             this.$onResize();
         }
     });
-
 });
